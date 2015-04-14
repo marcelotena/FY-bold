e669c96dbfdc7992c80909dce5b663c53772bee8
Marcelo Tena Vicente <marcelotena@gmail.com>
2015-04-14 09:41:06 +0200

    Added masonry library for portfolio page

--- js/script.js
+++ js/script.js
@@ -2196,6 +2196,14 @@ $(function() {
         }
     });
     
+    //load masonry for portfolio page
+    var $container = $('#masonry-portfolio'); 
+    $container.masonry({
+        columnWidth: 500,
+        itemSelector: '.project-container',
+        isAnimated: true
+    });
+    
     function isHighDensity(){
         return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 124dpi), only screen and (min-resolution: 1.3dppx), only screen and (min-resolution: 48.8dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (min-device-pixel-ratio: 1.3)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 1.3));
     }
